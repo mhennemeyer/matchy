2e00b6fb3dc29291d6819ebc1f6d0b6ce7d898ee
Matthias Hennemeyer <mhennemeyer@noname.(none)>
2010-01-04 14:42:26 +0100

    matchy tests with new testing framework

--- test/matcher_system/script.rb
+++ test/matcher_system/script.rb
@@ -3,7 +3,16 @@ Dir[File.expand_path("#{dir}/**/*.rb")].uniq.each do |file|
   if (file =~ /\/test_\w+\.rb$/) && (file !~ /\/test_helper\.rb$/)
     puts file
     File.open(file,File::RDWR) do |f|
-      f.readlines
+      new_lines = []
+      old_lines = f.readlines
+      old_lines.each do |line|
+        if line =~ /\A\s*def\stest_(.*)\Z/
+          desc = $1.gsub(/\s/, "").gsub(/_/, " ")
+          line = "test \"#{desc}\""
+        end
+        new_lines << line
+      end
+      f.puts new_lines
     end
   end
 end
