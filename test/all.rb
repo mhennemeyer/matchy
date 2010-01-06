99bc6a6d7f55c9193498c16169339a5b93508d3b
Matthias Hennemeyer <mhennemeyer@noname.(none)>
2010-01-06 17:13:36 +0100

    Refactor

--- test/all.rb
+++ test/all.rb
@@ -1,7 +1,7 @@
 dir = File.dirname(__FILE__)
 Dir[File.expand_path("#{dir}/**/*.rb")].uniq.each do |file|
   if (file =~ /\/test_\w+\.rb$/) && (file !~ /\/test_helper\.rb$/)
-    puts file
+    # puts file
     require file 
   end
 end
